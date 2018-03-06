87fed90ba40f7bb23c875187a9aaedecff156e1b
David Britch <davidbritch@Davids-MacBook-Pro.local>
2018-03-06 10:03:03 +0000

    Updated all code, aside from the UWP project, to use .NET Standard 1.0.

--- Library/Behaviors/ActionCollection.cs
+++ Library/Behaviors/ActionCollection.cs
@@ -4,39 +4,39 @@ using Xamarin.Forms;
 
 namespace Behaviors
 {
-	[Preserve(AllMembers = true)]
-	public class ActionCollection : BindableObjectCollection
-	{
-		public ActionCollection()
-		{
-			CollectionChanged += ActionCollection_CollectionChanged;
-		}
+    [Preserve(AllMembers = true)]
+    public class ActionCollection : BindableObjectCollection
+    {
+        public ActionCollection()
+        {
+            CollectionChanged += ActionCollection_CollectionChanged;
+        }
 
-		void ActionCollection_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
-		{
-			var collectionChange = e.Action;
+        void ActionCollection_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            var collectionChange = e.Action;
 
-			if (collectionChange == NotifyCollectionChangedAction.Reset)
-			{
-				foreach (BindableObject bindable in this)
-				{
-					ActionCollection.VerifyType(bindable);
-				}
-			}
-			else if (collectionChange == NotifyCollectionChangedAction.Replace)
-			{
-				BindableObject changed = this[(int)e.NewStartingIndex];
-				ActionCollection.VerifyType(changed);
-			}
-		}
+            if (collectionChange == NotifyCollectionChangedAction.Reset)
+            {
+                foreach (BindableObject bindable in this)
+                {
+                    ActionCollection.VerifyType(bindable);
+                }
+            }
+            else if (collectionChange == NotifyCollectionChangedAction.Replace)
+            {
+                BindableObject changed = this[(int)e.NewStartingIndex];
+                ActionCollection.VerifyType(changed);
+            }
+        }
 
-		static void VerifyType(BindableObject bindable)
-		{
-			if (!(bindable is IAction))
-			{
-				throw new InvalidOperationException("Non-IAction added to IAction collection");
-			}
-		}
-	}
+        static void VerifyType(BindableObject bindable)
+        {
+            if (!(bindable is IAction))
+            {
+                throw new InvalidOperationException("Non-IAction added to IAction collection");
+            }
+        }
+    }
 }
 
